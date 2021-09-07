b378a5afeaa00721bcd7a31498d07b2c3d3b0828
Documenter.jl <documenter@juliadocs.github.io>
2021-09-07 06:20:01 +0000

    build based on 913369a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RealTimeQST","category":"page"},{"location":"#RealTimeQST","page":"Home","title":"RealTimeQST","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RealTimeQST.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RealTimeQST]","category":"page"}]
+}
